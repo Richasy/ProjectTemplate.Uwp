7a5ab24aa0fb224be5491da84884243c5c47496e
Richasy <Thansy@foxmail.com>
2021-04-13 09:00:27 +0800

    Change namespace

--- RichasyModule/Implementation/Implementation.cs
+++ RichasyModule/Implementation/Implementation.cs
@@ -3,7 +3,7 @@
 using System;
 using $ext_safeprojectname$.Interfaces;
 
-namespace Microsoft.Media.Player.$ext_safeprojectname$
+namespace Richasy.$ext_safeprojectname$
 {
     /// <inheritdoc />
     public class $ext_safeprojectname$ : I$ext_safeprojectname$
